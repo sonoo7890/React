6554c47eab9c42b2f4d34f245c4889896bdc06fb
Adarsh Gour <adarshgour082@gamil.com>
2026-01-18 18:34:36 +0530

    added PropsChild file

---
+++ src/PropChild.jsx
@@ -0,0 +1,29 @@
+//  short (rafce),destructuring,import,export
+
+    // const PropChild =(props)=>{
+     // const PropChild =(myname,myage)=>{
+     //    console.log(props);
+    // console.log(props)
+    const PropChild=({myimg,myprice,mydes})=>{
+
+    // let {myname,myage}=props  //Destructuring
+
+    return(
+        
+        <div>
+
+            <div id='card'>
+                <img src={myimg} alt="" width="100%" height="200px"/>
+                <h1>Price 999</h1>
+                <h3>{mydes}</h3>
+                <button>Buy Now</button>
+
+            </div>
+            {/* this is PropChild
+            <h1>my name is {props.myname} </h1> */}
+            {/* <h1>my name is {myname}and my age is {myage} </h1>  */}
+
+        </div>
+    )
+}
+export default PropChild
