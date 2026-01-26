87e57ff599a5fba81e79c08609d52634f7813475
Adarsh Gour <adarshgour082@gamil.com>
2026-01-26 17:48:00 +0530

    added Footer file

---
+++ src/Footer.jsx
@@ -0,0 +1,18 @@
+let Footer1=()=>{
+ return(
+    <div>
+        <h1>This is Footer 1</h1>
+    </div>
+ )
+
+}
+let Footer2=()=>{
+ return(
+    <div>
+        <h1>This is Footer 2</h1>
+    </div>
+ )
+
+}
+
+export {Footer1,Footer2}
